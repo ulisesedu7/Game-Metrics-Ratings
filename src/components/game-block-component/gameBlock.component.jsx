0d10a4bacb95bdc7045135af24a7d1c4ff540035
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-07-26 15:24:30 -0600

    Set up initial componets to display the information

---
+++ src/components/game-block-component/gameBlock.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { useDispatch } from 'react-redux';
+
+const GameBlock = (props) => {
+  const { title, image } = props;
+
+  // const dispatch = useDispatch();
+
+  return (
+    <section className="game-container">
+      <div className="image-container">
+        <img src={image} alt="Picture of a game" />
+      </div>
+
+      <h2 className="game-title">{title}</h2>
+    </section>
+  );
+};
+
+GameBlock.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+export default GameBlock;
--- src/services/gamesApi.js
+++ src/services/gamesApi.js
@@ -1,16 +1,31 @@
+/* eslint-disable camelcase */
 import { fetchGames } from '../redux/games/games.reducer';
 
 // Base URL
-const baseUrl = 'https://api.rawg.io/api/games?key=beb4844ef73a431fb1c24f5674e2d9a7';
+// const baseUrl = 'https://api.rawg.io/api/games?key=beb4844ef73a431fb1c24f5674e2d9a7';
+
+const baseUrl2 = 'https://api.rawg.io/api/games?key=beb4844ef73a431fb1c24f5674e2d9a7&page_size=1';
 
 // API Action
 const getGamesFromApi = () => async (dispatch) => {
-  const data = await fetch(baseUrl);
+  const data = await fetch(baseUrl2);
   const response = await data.json();
 
   const gamesList = response;
 
-  dispatch(fetchGames(gamesList));
+  console.log(gamesList);
+
+  dispatch(
+    fetchGames(
+      gamesList.map(({
+        id, name, background_image,
+      }) => ({
+        id,
+        gameName: name,
+        image: background_image,
+      })),
+    ),
+  );
 
   return null;
 };
